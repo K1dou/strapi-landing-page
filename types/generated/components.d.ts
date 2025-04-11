4b6e2c7dee036248cf5c012f14bf1257a98f7446
K1dou <hique1276@gmail.com>
2025-04-10 22:29:13 -0300

    Add menu components and update page schema to include footer text

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,39 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface MenuMenu extends Struct.ComponentSchema {
+  collectionName: 'components_menu_menus';
+  info: {
+    description: '';
+    displayName: 'menu';
+  };
+  attributes: {
+    logo: Schema.Attribute.Media<'images'>;
+    logo_link: Schema.Attribute.Text & Schema.Attribute.Required;
+    logo_text: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 255;
+        minLength: 3;
+      }>;
+    menu_links: Schema.Attribute.Component<'menu.menu-link', true> &
+      Schema.Attribute.Required;
+  };
+}
+
+export interface MenuMenuLink extends Struct.ComponentSchema {
+  collectionName: 'components_menu_menu_links';
+  info: {
+    displayName: 'menu_link';
+  };
+  attributes: {
+    link_text: Schema.Attribute.String & Schema.Attribute.Required;
+    open_in_new_tab: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    url: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
 export interface SectionImageGrid extends Struct.ComponentSchema {
   collectionName: 'components_section_image_grids';
   info: {
@@ -160,6 +194,8 @@ export interface SharedSlider extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'menu.menu': MenuMenu;
+      'menu.menu-link': MenuMenuLink;
       'section.image-grid': SectionImageGrid;
       'section.section-content': SectionSectionContent;
       'section.section-grid': SectionSectionGrid;
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -445,6 +445,7 @@ export interface ApiPagePage extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    footer_text: Schema.Attribute.Blocks & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::page.page'> &
       Schema.Attribute.Private;
